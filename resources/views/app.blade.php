7ad8edcf06346905afc9ebf8ba7f633bea24caa9
root <root@1323900-cn37575.tw1.ru>
2023-06-30 10:50:13 +0300

    add logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/png" href="<%= BASE_URL %>favicon.svg">
 	<title>AdminPanel</title>
 
 	@vite(['resources/css/app.css', 'resources/js/app.js'])</head>
