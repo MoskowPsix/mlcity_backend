a4cf1e3dc6b5ff696a62029338eadf0fe4869558
MoskowPsix <bolshe.kivi@gmail.com>
2023-07-12 11:20:17 +0500

    add functions vk post

--- resources/js/components/event/Event.vue
+++ resources/js/components/event/Event.vue
@@ -142,7 +142,7 @@ useEventsStore().getTypes();
 
     <div class="flex items-center justify-between mt-6">
         <div v-for="link in event_store.links">
-            <div v-if=" link.label  === pageP">
+            <div v-if="link.label  === pageP && link.url !== null">
                 <a href="#/events" @click="event_store.getEventUrl(link.url)" class="flex items-center px-5 mx-2 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:-scale-x-100">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
@@ -164,7 +164,7 @@ useEventsStore().getTypes();
                 </div>
             </div>
 
-            <div v-if="link.label === pageN">
+            <div v-if="link.label === pageN && link.url !== null">
                 <a href="#/events" @click="event_store.getEventUrl(link.url)" class="flex items-center mx-2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
                     <span>
                         Вперёд
