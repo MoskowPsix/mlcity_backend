0820e9e5411c83179050235d5344fb16e94f1369
MoskowPsix <bolshe.kivi@gmail.com>
2023-08-08 12:36:51 +0500

    fix: routes api

--- routes/api.php
+++ routes/api.php
@@ -35,6 +35,7 @@ Route::controller(AuthController::class)->group(function() {
     Route::post('logout/{id}', 'logout')->middleware('auth:sanctum');
 });
 
+
 Route::controller(UserController::class)->group(function() {
     Route::get('listUsers/', 'listUsers')->middleware('auth:sanctum'); // Для админ панели(поиск юзера по фильтрам)
     Route::put('updateUsers/{id}/', 'updateUsers')->middleware('auth:sanctum'); // Для админ панели(изменить инфу о юзере)
@@ -63,13 +64,15 @@ Route::controller(EventController::class)->group(function() {
     Route::post('events/update-vk-likes', 'updateVkLikes');//для страницы мероприятия
     Route::post('events/set-event-user-liked', 'setEvenUserLiked')->middleware('auth:sanctum');//для страницы мероприятия
     Route::get('events/{id}', 'show');
+
     Route::get('events/{id}/check-user-liked', 'checkLiked')->middleware('auth:sanctum');// Проверяем лайкал ли юзер ивент
     Route::get('events/{id}/check-user-favorite', 'checkFavorite')->middleware('auth:sanctum');// Проверяем добавил ли юзер в избранное
+
     Route::post('events/create', 'create')->middleware('auth:sanctum');
     Route::put('updateEvent/{id}/', 'updateEvent')->middleware('auth:sanctum');
+
     Route::get('events/{id}/liked-users', 'getEventUserLikedIds')->middleware('auth:sanctum');
     Route::get('events/{id}/favorites-users', 'getEventUserFavoritesIds')->middleware('auth:sanctum');
-    Route::get('ev', 'postEvent');
     //Route::delete('events/{id}', 'delete')->middleware('auth:sanctum');
 });
 
@@ -112,10 +115,8 @@ Route::controller(StatusController::class)->group(function() {
     Route::get('getStatusId/{id}', 'getStatusId');
     // Для событий
     Route::post('events/addStatusEvent', 'addStatusEvent')->middleware('auth:sanctum');
-    Route::delete('events/deleteStatusEvent/{event_id}/{status_id}', 'deleteStatusEvent')->middleware('auth:sanctum');
     //Для достопримечательностей
     Route::post('sights/addStatusSight', 'addStatusSight');
-    Route::delete('sights/deleteStatusSight/{sight_id}/{status_id}', 'deleteStatusSight');
 
 });
 
