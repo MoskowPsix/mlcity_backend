5fec690bbc97f63bdae6cef6ae99da73fc699d81
MoskowPsix <bolshe.kivi@gmail.com>
2023-09-06 17:47:13 +0500

    fix command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('addView')->everyMinute();
+        $schedule->command('addView')->twiceDaily(1, 13);;
     }
 
     /**
