f58f1dc605567c35845c6155f56f538ed05a8131
MoskowPsix <bolshe.kivi@gmail.com>
2023-11-08 15:22:19 +0500

    fix command db backup

--- app/Console/Commands/backupPSQL.php
+++ app/Console/Commands/backupPSQL.php
@@ -35,8 +35,9 @@ class backupPSQL extends Command
     {
         $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".gz";
   
-        $command = "pg_dump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . storage_path() . "/app/backup/" . $filename;
+        // $command = "pg_dump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . storage_path() . "/app/backup/" . $filename;
   
+        $command = "pg_dump -U " . env('DB_USERNAME') ." -W" . env('DB_PASSWORD') . " -h " . env('DB_HOST') . " -d " . env('DB_DATABASE') . " -F directory -j 4 | gzip > /app/backup/" . $filename;
         $returnVar = NULL;
         $output  = NULL;
   
