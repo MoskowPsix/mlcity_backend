1dc0ff376c0254d04243812e3fb6a56c8ef4256e
MoskowPsix <bolshe.kivi@gmail.com>
2023-11-08 13:27:43 +0500

    add command backup_db

---
+++ app/Console/Commands/backupPSQL.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+
+class backupPSQL extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'backup_db';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Weekly PostgresSQL Backup';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".gz";
+  
+        $command = "pg_dump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . storage_path() . "/app/backup/" . $filename;
+  
+        $returnVar = NULL;
+        $output  = NULL;
+  
+        exec($command, $output, $returnVar);
+    }
+}
