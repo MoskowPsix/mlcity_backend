99ec370dba016bc425bec91ede5d9723131a3b66
MoskowPsix <bolshe.kivi@gmail.com>
2023-12-17 13:43:19 +0500

    fix error in command institutes

--- app/Console/Commands/addInstitutes.php
+++ app/Console/Commands/addInstitutes.php
@@ -9,6 +9,9 @@ use App\Models\SightType;
 use App\Models\Sight;
 use App\Models\Status;
 use App\Models\Location;
+use Exception;
+use Illuminate\Support\Facades\Log;
+
 
 class addInstitutes extends Command
 {
@@ -36,20 +39,56 @@ class addInstitutes extends Command
         function searchLevelLocales($max_level) {
             $request = json_decode(file_get_contents('https://www.culture.ru/api/locales?level='.$max_level.'&limit=1', TRUE));
             if ($request->total === 0) {
-                //$output->writeln('<info>' . $max_level . '</info>');
-                //echo $max_level - 1;
                 $max_level = $max_level - 1;
-                //return (int)$max_level;
             } else if ($request->total !== 0) {
-                //print_r($max_level);
                 $max_level = $max_level + 1;
                 searchLevelLocales($max_level);
             }
             return (int)$max_level;
         }
-        function getMessage($text) {
-            file_get_contents('https://api.telegram.org/bot'.env('TELEGRAM_BOT_API').'/sendMessage?chat_id='.env('LOG_CHATS_DOWNLOAD_TELEGRAM').'&text='. $text);
+        function  getPageInstitutes($page_institutes, $limit_institutes) {
+            try
+            {
+                $response = json_decode(file_get_contents('https://www.culture.ru/api/institutes?page='.$page_institutes.'&limit='.$limit_institutes . '&statuses=published', true));
+                return $response;
+            }  catch (Exception $e) {
+                Log::error('Ошибка при получении страницы events(page='.$page_institutes.', limit='.$limit_institutes.'): '.json_decode($e));
+                sleep(5);
+                getPageEvent($page_institutes, $limit_institutes);
+            }    
         }
+        function getInstitute($id) {
+            try
+            {
+                $response = json_decode(file_get_contents('https://www.culture.ru/api/institutes/' .  $id . '?fields=thumbnailFile', TRUE));
+                return $response;
+            }  catch (Exception $e) {
+                Log::error('Ошибка при получении института '.$id.': '.json_decode($e));
+                sleep(5);
+                getInstitute($id);
+            }    
+        }
+        function getRubricsPage($limit_rubrics, $page_rubrics) {
+            try
+            {
+                $response = json_decode(file_get_contents('https://www.culture.ru/api/rubrics?sort=level&limit='.$limit_rubrics.'&page=' . $page_rubrics, true));
+                return $response;
+            }  catch (Exception $e) {
+                Log::error('Ошибка при получении страницы рубрик(page='.$limit_rubrics.', limit='.$page_rubrics.'): '.json_decode($e));
+                sleep(5);
+                getRubricsPage($limit_rubrics, $page_rubrics);
+            }    
+        }
+
+        function getMessage($text) {
+            try
+            {
+                file_get_contents('https://api.telegram.org/bot'.env('TELEGRAM_BOT_API').'/sendMessage?chat_id='.env('LOG_CHATS_DOWNLOAD_TELEGRAM').'&text='. $text);
+            }  catch (Exception $e) {
+                Log::error('Ошибка при отправке сообщения в телеграм: '.json_decode($e));
+                sleep(5);
+                getMessage($text);
+            }                  }
 
         $output = new \Symfony\Component\Console\Output\ConsoleOutput();
         $page_institutes = 1;
@@ -66,16 +105,13 @@ class addInstitutes extends Command
         $total_rubric_progress = $total_rubrics / 100;
 
         $limit_locations = 10;
-        $locations_download = [];
         $level_max_locations = searchLevelLocales(1);
         $level_locations = 1;
-
-        $total = 10;
         
 
         $output->writeln('Download start element-1');
         $output->writeln('Download step 1(max '.$level_max_locations.' level locations): download locations');
-        getMessage('<info>Download start element-1</info>');
+        getMessage('Download start element-1');
         getMessage('Download step 1(max '.$level_max_locations.' level locations): download locations');
         $null_location = json_decode(file_get_contents('https://www.culture.ru/api/locales/1', true));
         if (!Location::where('cult_id', $null_location->_id)->first()) {
@@ -85,35 +121,28 @@ class addInstitutes extends Command
                 'cult_id' => $null_location->_id
             ]);
         }
-        while ($level_max_locations >= $level_locations) {
-            $page_locations = 1;
-            $total_locations = json_decode(file_get_contents('https://www.culture.ru/api/locales?limit='.$limit_locations.'&page=' . $page_locations . '&level=' . $level_locations, true))->pagination->total;
-            $total_locations_progress = $total_locations / 100;
-            $output->writeln('Level '.$level_locations.' locations start:');
-            getMessage('Level '.$level_locations.' locations start:');
-            while ($total_locations >= 0) {
-                // Отображение прогресса
-                $progress = ($total_locations_progress * 100 - $total_locations) / $total_locations_progress;
-                // $output->writeln((int)$progress . '%');
-                // getMessage($progress . '%');
-
-                $locales = json_decode(file_get_contents('https://www.culture.ru/api/locales?&limit='.$limit_locations.'&page=' . $page_locations . '&level=' . $level_locations, true));
-                foreach ($locales->items as $local) {
-                    if (!Location::where('cult_id', $local->_id)->first()) {
-                        //print_r($local);
-                        Location::create([
-                            'name' => $local->title,
-                            'time_zone' => $local->timezone,
-                            'cult_id' => $local->_id,
-                            'location_id' => Location::where('cult_id', (int)$local->parentId)->firstOrFail()->id
-                        ]);
-                    } 
-                }
-                $total_locations = $total_locations - 1;
-                $page_locations = $page_locations + 1;
-            }
-            $level_locations = $level_locations + 1;
-        }   
+        // while ($level_max_locations >= $level_locations) {
+        //     $page_locations = 1;
+        //     $total_locations = json_decode(file_get_contents('https://www.culture.ru/api/locales?limit='.$limit_locations.'&page=' . $page_locations . '&level=' . $level_locations, true))->pagination->total;
+        //     $output->writeln('Level '.$level_locations.' locations start:');
+        //     getMessage('Level '.$level_locations.' locations start:');
+        //     while ($total_locations >= 0) {
+        //         $locales = json_decode(file_get_contents('https://www.culture.ru/api/locales?&limit='.$limit_locations.'&page=' . $page_locations . '&level=' . $level_locations, true));
+        //         foreach ($locales->items as $local) {
+        //             if (!Location::where('cult_id', $local->_id)->first()) {
+        //                 Location::create([
+        //                     'name' => $local->title,
+        //                     'time_zone' => $local->timezone,
+        //                     'cult_id' => $local->_id,
+        //                     'location_id' => Location::where('cult_id', (int)$local->parentId)->firstOrFail()->id
+        //                 ]);
+        //             } 
+        //         }
+        //         $total_locations = $total_locations - 1;
+        //         $page_locations = $page_locations + 1;
+        //     }
+        //     $level_locations = $level_locations + 1;
+        // }   
 
 
         $output->writeln('Download step 2: download rubrics');
@@ -124,7 +153,7 @@ class addInstitutes extends Command
             //$output->writeln((int)$progress . '%');
             //getMessage((int)$progress . '%');
 
-            $rubrics = json_decode(file_get_contents('https://www.culture.ru/api/rubrics?sort=level&limit='.$limit_rubrics.'&page=' . $page_rubrics, true));
+            $rubrics = getRubricsPage($limit_rubrics, $page_rubrics);
             foreach ($rubrics->items as $rubric) {
                 if (!$rubric->parentId){
                     if(!SightType::where('cult_id', $rubric->_id)->first()) {
@@ -215,7 +244,7 @@ class addInstitutes extends Command
             $progress = ($total_institutes_progress * 100 - $total_institutes) / $total_institutes_progress;
             $output->writeln((int)$progress . '%');
 
-            $sights = json_decode(file_get_contents('https://www.culture.ru/api/institutes?page='.$page_institutes.'&limit='.$limit_institutes . '&statuses=published', true));
+            $sights = getPageInstitutes($page_institutes, $limit_institutes);
             foreach ($sights->items as $sight) {
                 if (!Sight::where('cult_id', $sight->_id)->first() && $sight->status !== 'deleted') {
                     // Берём тип
@@ -286,7 +315,7 @@ class addInstitutes extends Command
                         // Ставим тип
                         Sight::where('cult_id', $sight->_id)->first()->types()->attach($types_id);
                     }
-                    $sight_one = json_decode(file_get_contents('https://www.culture.ru/api/institutes/' .  $sight->_id . '?fields=thumbnailFile', TRUE));
+                    $sight_one = getInstitute($sight->_id);
                     // Подвязываем фото
                     if (isset($sight_one->thumbnailFile)) {
                         Sight::where('cult_id', $sight->_id)->first()->files()->create([
