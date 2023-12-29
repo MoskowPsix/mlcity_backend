77b3080d937c50fac4d58837fd454b9ee59282b0
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2023-12-29 15:11:53 +0500

    write modal 1/3

---
+++ resources/js/components/search_type/searchType.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="h-3/4 w-3/4 bg-white rounded-lg">
+        <form class="p-4">
+            <div class="space-x-4 p-2">
+                <div class="border-gray-500 border-b rounded p-2 flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+
+                    
+
+                    <input v-model="searchText" @input="searchType" class=" ml-2 focus:outline-none w-full" placeholder="Поиск типов">
+                </div>
+            </div>
+
+            <div>
+                <p class="text-sm ml-2 text-gray-400">Текст поиска: {{ searchText }}</p>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+import { useTypeStore } from '../../stores/TypeStore'
+import { mapActions } from 'pinia'
+import { catchError, map, retry, delay, takeUntil} from 'rxjs/operators'
+
+export default{
+    name:"searchType",
+    data(){
+        return {
+            searchText: "",
+            timeout: null
+        }
+    },
+    methods:{
+        ...mapActions(useTypeStore,['getTypeByText']),
+        searchType(){
+            // console.log(this.timeout)
+            if (this.timeout){
+                clearTimeout(this.timeout)
+            }
+            this.timeout = setTimeout(() => {
+            this.getType()
+            }, 2000)  
+        },
+
+        getType(){
+            let params = {
+                name: this.searchText
+            }
+            console.log(params)
+            this.getTypeByText(params, "Sight").pipe(
+                retry(3),
+                delay(200),
+                catchError(error => {
+                    console.log(error)
+                })
+            ).subscribe(response => console.log(response))
+        }
+    }
+}
+</script>
+<style>
+input:focus {
+  border: none;
+}
+</style>
