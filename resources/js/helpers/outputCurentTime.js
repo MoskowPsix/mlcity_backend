08997dd9875085b8e6573e4f803dee8fa4734274
MoskowPsix <bolshe.kivi@gmail.com>
2024-01-08 21:22:48 +0500

    add timezone

--- resources/js/helpers/outputCurentTime.js
+++ resources/js/helpers/outputCurentTime.js
@@ -1,9 +1,13 @@
 import { useLocalStorage } from "@vueuse/core";
+import moment from 'moment'
+import 'moment-timezone'
 
 
 export default {
     // Функция вывода времени с учётом временной зоны пользователя
-    outputCurentTime(time) {
-        return new Date(time).toLocaleString("ru-RU", {timeZone: useLocalStorage().timeZone});
+    outputCurentTime(time, timeZone) {
+        let data = moment(time, "YYYY-MM-DD HH:mm:ss").tz(timeZone)
+        console.log(time)
+        return data.format("YYYY-MM-DD HH:mm:ss")
     }
 }
