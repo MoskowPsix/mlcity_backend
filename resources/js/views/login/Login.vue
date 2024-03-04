ea307f26841cb14c45ba89d622092895c10ba414
MoskowPsix <bolshe.kivi@gmail.com>
2024-03-04 13:39:11 +0500

    add auth vk

--- resources/js/views/login/Login.vue
+++ resources/js/views/login/Login.vue
@@ -5,10 +5,9 @@ import { useLocalStorageStore } from '../../stores/LocalStorageStore'
 import { useLoaderStore } from '../../stores/LoaderStore'
 import { MessageAuth } from '../../enums/auth_messages'
 import { useToastStore } from '../../stores/ToastStore'
-import { catchError, tap, map, retry, delay, takeUntil} from 'rxjs/operators'
+import { catchError, map, takeUntil} from 'rxjs/operators'
 import { of, EMPTY, Subject } from 'rxjs'
 import router from '../../routes'
-import axios from 'axios'
 
 
 export default {
@@ -66,35 +65,11 @@ export default {
         this.setToken(JSON.parse(JSON.stringify(this.$route.params.token)))
         this.setTimeZone(Intl.DateTimeFormat().resolvedOptions().timeZone)
         this.localStorageInit()
-        // this.closeLoaderFullPage()
         router.go({path: 'user/events' })
-        // axios.defaults.headers = {'Authorization': `Bearer ${this.$route.params.token.length}`}
-
-        // await this.getUserForToken().pipe(
-        //   map(async response => {
-        //     await this.setToken(JSON.stringify(this.$route.params.token))
-        //     await this.setUser(response.data.user)
-        //     await response.data.user.roles[0] ? this.setRole(response.data.user.roles[0].name) : null
-        //     await this.setTimeZone(Intl.DateTimeFormat().resolvedOptions().timeZone)
-        //     await this.closeLoaderFullPage()
-        //     router.go({path: 'user/events'})
-        //   }),
-        //   catchError(err => {
-        //     console.log(err)
-        //     if (399 < err.response.status && err.response.status < 500) {
-        //       this.showToast(MessageAuth.warning_auth, 'warning')
-        //     } else if(499 < err.response.status && err.response.status < 600) {
-        //       this.showToast(MessageAuth.error_auth, 'error')
-        //     }
-        //     this.closeLoaderFullPage()
-        //     localStorage.clear()
-        //     this.localStorageInit()
-        //     return of(EMPTY)
-        //   }),
-        // ).subscribe()
-        
-
       }
+    },
+    loginVK() {
+      window.location.href = `${import.meta.env.VITE_APP_URL}/api/social-auth/vkontakte`
     }
   },
   mounted() {
@@ -129,7 +104,17 @@ export default {
                           </div>
                       </div>
                   </div>
-                  <button @click.prevent="loginSubmit()" class="w-full dark:text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Войти</button></form>
+                  <button @click.prevent="loginSubmit()" class="w-full dark:text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Войти</button>
+                  <button @click.prevent="loginVK()" class="w-full dark:text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    <div class="grid grid-cols-2 content-stretch w-auto">
+                      <p>Войти с помощью</p>
+                      <svg fill="white" width="25" height="25" viewBox="-2.5 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" class="w-full">
+                        <title>vk</title>
+                        <path d="M16.563 15.75c-0.5-0.188-0.5-0.906-0.531-1.406-0.125-1.781 0.5-4.5-0.25-5.656-0.531-0.688-3.094-0.625-4.656-0.531-0.438 0.063-0.969 0.156-1.344 0.344s-0.75 0.5-0.75 0.781c0 0.406 0.938 0.344 1.281 0.875 0.375 0.563 0.375 1.781 0.375 2.781 0 1.156-0.188 2.688-0.656 2.75-0.719 0.031-1.125-0.688-1.5-1.219-0.75-1.031-1.5-2.313-2.063-3.563-0.281-0.656-0.438-1.375-0.844-1.656-0.625-0.438-1.75-0.469-2.844-0.438-1 0.031-2.438-0.094-2.719 0.5-0.219 0.656 0.25 1.281 0.5 1.813 1.281 2.781 2.656 5.219 4.344 7.531 1.563 2.156 3.031 3.875 5.906 4.781 0.813 0.25 4.375 0.969 5.094 0 0.25-0.375 0.188-1.219 0.313-1.844s0.281-1.25 0.875-1.281c0.5-0.031 0.781 0.406 1.094 0.719 0.344 0.344 0.625 0.625 0.875 0.938 0.594 0.594 1.219 1.406 1.969 1.719 1.031 0.438 2.625 0.313 4.125 0.25 1.219-0.031 2.094-0.281 2.188-1 0.063-0.563-0.563-1.375-0.938-1.844-0.938-1.156-1.375-1.5-2.438-2.563-0.469-0.469-1.063-0.969-1.063-1.531-0.031-0.344 0.25-0.656 0.5-1 1.094-1.625 2.188-2.781 3.188-4.469 0.281-0.5 0.938-1.656 0.688-2.219-0.281-0.625-1.844-0.438-2.813-0.438-1.25 0-2.875-0.094-3.188 0.156-0.594 0.406-0.844 1.063-1.125 1.688-0.625 1.438-1.469 2.906-2.344 4-0.313 0.375-0.906 1.156-1.25 1.031z"></path>
+                      </svg>
+                    </div>
+                  </button>
+              </form>
           </div>
       </div>
   </div>
