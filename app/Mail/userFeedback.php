6fd52511ff7f63ca6577c2c0d863bccd4aa231f6
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2024-03-28 17:41:22 +0500

    getting user feedback

--- app/Mail/userFeedback.php
+++ app/Mail/userFeedback.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class Feedback extends Mailable
+class UserFeedback extends Mailable
 {
     use Queueable, SerializesModels;
 
