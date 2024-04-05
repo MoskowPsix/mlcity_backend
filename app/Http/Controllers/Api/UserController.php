e8096f6880991b51769c5bf4dda46f54ba211880
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2024-04-05 15:41:55 +0500

    fix agreement

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -865,7 +865,7 @@ class UserController extends Controller
         try{
             $user = auth('api')->user();
 
-            $agreement =  UserAgreement::where("title", $reqeust->get("type"))->first()->id;
+            $agreement =  UserAgreement::where("id", $reqeust->get("agreement_id"))->first()->id;
             $checkUserAgreementIsAlredy = $user->userAgreements()->where("user_agreement_id",$agreement)->get();
 
             if(count($checkUserAgreementIsAlredy) > 0){
