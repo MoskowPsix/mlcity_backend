1022efd4c8d222465b1c6ea3bff3f250989d05c6
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2024-05-03 15:38:28 +0500

    add new Filter

---
+++ app/Filters/Place/PlaceIds.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filters\Place;
+
+
+use Closure;
+use App\Filters\Pipe;
+
+class PlaceIds implements Pipe{
+    public function apply($content, Closure $next){
+        if (request()->has("placeIds")){
+            $placesIds = $this->parseIds(request()->get("placeIds"));
+            info($placesIds);
+            $content->whereIn("id", $placesIds);
+        }
+        return $next($content);
+    }
+
+    public function parseIds($array) {
+        $array = trim($array, "[");
+        $array = trim($array, "]");
+        $placeIds = explode(",", $array);
+
+        return $placeIds;
+    }
+}
