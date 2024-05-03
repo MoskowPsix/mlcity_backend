e0d20d0f90a05e0271b8fda6c967ea3325665b71
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2024-05-03 15:36:20 +0500

    gettings place by array of ids

--- app/Http/Controllers/Api/PlaceController.php
+++ app/Http/Controllers/Api/PlaceController.php
@@ -6,6 +6,7 @@ use App\Filters\Place\PlaceAddress;
 use App\Filters\Place\PlaceDate;
 use App\Filters\Place\PlaceGeoPositionInArea;
 use App\Filters\Place\PlaceIco;
+use App\Filters\Place\PlaceIds;
 use App\Filters\Place\PlaceTypes;
 use App\Filters\Place\PlaceStatuses;
 use App\Filters\Place\PlaceLocation;
@@ -22,41 +23,37 @@ class PlaceController extends Controller
 {
     public function getPlaces (Request $request): \Illuminate\Http\JsonResponse
     {
-        if (request()->has('radius') && ($request->radius <= 25) && (request()->get('latitude') && request()->get('longitude'))) {
+        $places = Place::query();
+        $response =
+            app(Pipeline::class)
+            ->send($places)
+            ->through([
+                PlaceGeoPositionInArea::class,
+                PlaceAddress::class,
+                PlaceDate::class,
+                PlaceTypes::class,
+                PlaceStatusesLast::class,
+                PlaceStatuses::class,
+                PlaceIco::class,
+                PlaceIds::class
+            ])
+            ->via('apply')
+            ->then(function ($places) {
+                $places = $places->get();
 
-            $places = Place::query();
-            $response =
-                app(Pipeline::class)
-                ->send($places)
-                ->through([
-                    PlaceGeoPositionInArea::class,
-                    PlaceAddress::class,
-                    PlaceDate::class,
-                    PlaceTypes::class,
-                    PlaceStatusesLast::class,
-                    PlaceStatuses::class,
-                    PlaceIco::class
-                ])
-                ->via('apply')
-                ->then(function ($places) {
-                    $places = $places->get();
-
-                    foreach($places as $key=>$place){
-                        $places[$key]->ico = $place->event->types[0]->ico;
-                        unset($places[$key]->event);
-                    }
-                    // foreach($places as $key=>$place){
-                    //     $type_id = DB::table("events_etypes")->where("event_id","=",$place->event_id)->first()->etype_id;
-                    //     $ico = EventType::find($type_id)->ico;
-                    //     $places[$key]->ico = $ico;
-                    // }
-                    return $places;
-                });
+                foreach($places as $key=>$place){
+                    $places[$key]->ico = $place->event->types[0]->ico;
+                    unset($places[$key]->event);
+                }
+                // foreach($places as $key=>$place){
+                //     $type_id = DB::table("events_etypes")->where("event_id","=",$place->event_id)->first()->etype_id;
+                //     $ico = EventType::find($type_id)->ico;
+                //     $places[$key]->ico = $ico;
+                // }
+                return $places;
+            });
 
             return response()->json(['status' => 'success', 'places' => $response], 200);
-        } else {
-            return response()->json(['status' => 'error arguments'], 400);
-        }
     }
 
     public function getPlacesIds($id) {
