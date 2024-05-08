9a747654b8e0dc970e5d9d9e8d222a0e1f9edb0f
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2024-05-08 19:01:14 +0500

    add new param

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Filters\Location\LocationDisplay;
 use App\Http\Controllers\Controller;
-use Illuminate\Contracts\Pipeline\Pipeline;
 use Illuminate\Http\Request;
 use App\Models\Location;
+use Illuminate\Pipeline\Pipeline;
 
 class LocationController extends Controller
 {
@@ -14,14 +15,26 @@ class LocationController extends Controller
         return response()->json(['status' => 'success', 'location' => $locations], 200);
     }
     public function getLocationsName($name) {
-        $locations = Location::orWhere('name', 'ilike', '%'.$name.'%')
-        ->orWhere('name', 'ilike', '%'.$name.'%')
+        $locations = Location::query()->orWhere('name', 'ilike', '%'.$name.'%')
         ->whereHas('locationParent', function($q)use($name){
             $q->orWhere('name', 'ilike', '%'.$name.'%');
         })
-        ->with('locationsChildren', 'locationParent')
-        ->get();
-        return response()->json(['status' => 'success', 'locations' => $locations], 200);
+        ->with('locationsChildren', 'locationParent');
+
+        $response = app(Pipeline::class)
+        ->send($locations)
+        ->through([
+            LocationDisplay::class
+        ])
+        ->via("apply")
+        ->then(function ($locations) {
+
+            $locations = $locations->get();
+            info($locations);
+            return $locations;
+        });
+
+        return response()->json(['status' => 'success', 'locations' => $response], 200);
     }
     public function searchLocationByCoords(Request $request) {
         $latitude = $request->latitude;
@@ -47,7 +60,7 @@ class LocationController extends Controller
         $locations = Location::where('location_id')->with('locationsChildren')->get();
         return response()->json(['status' => 'success', 'locations' => $locations], 200);
     }
-    public function getLocationsAndRegion(Request $request) 
+    public function getLocationsAndRegion(Request $request)
     {
         $region = Location::where('name', 'LIKE', '%' . $request->parentName . '%')->first();
         $locations = Location::where('name', 'LIKE' , '%' . $request->name . '%')->where('location_id', $region->id)->with('locationParent')->first();
