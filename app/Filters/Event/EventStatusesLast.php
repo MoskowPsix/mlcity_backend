efe9670324b9bd6eb0bb897ec1432ff00cd85590
MoskowPsix <bolshe.kivi@gmail.com>
2024-06-03 17:14:44 +0500

    fix for admin

--- app/Filters/Event/EventStatusesLast.php
+++ app/Filters/Event/EventStatusesLast.php
@@ -11,9 +11,18 @@ class EventStatusesLast implements Pipe {
 
     public function apply($content, Closure $next)
     {
-        if(true){
+        if(request()->has('statuses') && request()->has('statusLast')){
             $statuses = explode(',', request()->get('statuses'));
-            $status_id = Status::where('name', $statuses[0])->first()->id;
+            info($statuses);
+            // Клиент присылает id статуса
+            if((is_numeric($statuses[0]))) {
+                info($statuses[0]);
+                $status_id = Status::find($statuses[0])->id;
+            }
+            // Админ присылает название статуса 
+            else {
+                $status_id = Status::where('name', $statuses[0])->first()->id;
+            }
             $content->whereHas('statuses', function($q) use ($status_id){
                 $q->where('last', true)->where('status_id', $status_id);
             });
