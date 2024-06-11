94dab372b4e17e554aeb30db6263148c201f3a3d
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2024-06-11 15:51:58 +0500

    fix import

--- resources/js/components/my_events_grid/MyEventsGrid.vue
+++ resources/js/components/my_events_grid/MyEventsGrid.vue
@@ -19,7 +19,7 @@
                     @click="alert(1)"
                 >
                     <img
-                        src="/storage/app/public/icons/edit_pen.svg"
+                        src="/storage/icons/edit_pen.svg"
                         alt=""
                     />
                 </div>
