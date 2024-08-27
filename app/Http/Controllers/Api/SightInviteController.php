78b84d3f805641a5afeb2052160e96b8b5e2f3a9
Ada-lave <mega.kefi36@gmail.com>
2024-08-27 07:54:26 +0300

    rename in Organization Invite

--- app/Http/Controllers/Api/SightInviteController.php
+++ app/Http/Controllers/Api/SightInviteController.php
@@ -3,20 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Organization;
+use App\Models\Sight;
 use App\Models\SightInvite;
 use Illuminate\Http\Request;
 
-class OrganizationInviteController extends Controller
+class SightInviteController extends Controller
 {
     public function acceptInvite(Request $request){
         $invite = SightInvite::where("token", $request->token)->first();
-        $organization = Organization::find($invite->sight_id);
+        $sight = Sight::find($invite->sight_id);
 
         $permissions = $invite->userPermissions;
-        info($permissions->toArray());
         foreach($permissions as $permission){
-            $organization->users()->attach($invite->user_id, [
+            $sight->users()->attach($invite->user_id, [
                 "permission_id" => $permission->id
             ]);
         }
