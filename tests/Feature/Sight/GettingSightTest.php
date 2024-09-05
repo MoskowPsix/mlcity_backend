3f5118690e5c9ded12a507e4fbe4abcf33a6322c
Ada-lave <mega.kefi36@gmail.com>
2024-09-05 15:53:37 +0300

    testing get sight

--- tests/Feature/Sight/GettingSightTest.php
+++ tests/Feature/Sight/GettingSightTest.php
@@ -2,21 +2,38 @@
 
 namespace Tests\Feature\Sight;
 
+use App\Models\Sight;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class GettingSightTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    protected $seed = true;
+
+    public function test_sight_by_id()
     {
-        $response = $this->get('/');
+        $sight = Sight::first();
+        $response = $this->getJson('api/sights/' . $sight->id);
 
         $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            "status",
+            "message",
+            "sight" => [
+                "id",
+                "name",
+                "files"
+            ]
+        ]);
+
+        $response->assertJsonFragment([
+            "name" => $sight->name,
+            "id" => $sight->id,
+            "files" => $sight->files
+        ]);
     }
 }
